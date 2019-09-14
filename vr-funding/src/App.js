3ddfc16290c1d9e1a95285e33f502844468487b6
Cory Hale <corylhale2013@gmail.com>
2019-09-14 12:48:05 -0600

    Added auth-middleware, actions, and reducers.

--- vr-funding/src/App.js
+++ vr-funding/src/App.js
@@ -7,15 +7,19 @@ import Dreamers from './pages/Dreamers';
 import Login from './pages//Login';
 import Register from './pages/Register';
 import Investors from './pages/Investors';
+import PrivateRoute from './components/Auth/PrivateRoute';
 
 function App() {
   return (
     <Router>
+      <header className="header">
+        <h1>VR Funding</h1>
+      </header>
       <Route path="/" exact component={Home} />
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
-      <Route path="/dreamers" component={Dreamers} />
-      <Route path="/investors" component={Investors} />
+      <PrivateRoute exact path="/dreamers" component={Dreamers} />
+      <PrivateRoute exact path="/investors" component={Investors} />
     </Router>
   );
 }
--- vr-funding/src/index.js
+++ vr-funding/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>
+, document.getElementById('root'));
